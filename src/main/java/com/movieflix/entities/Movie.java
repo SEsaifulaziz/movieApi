3fc78c31340aef8be41d9d35fddebe0d0650c9b6
Saiful Aziz <gamestopik712@gmail.com>
2026-02-27 01:40:27 +0500

    add Movie Entity & MovieRepository

---
+++ src/main/java/com/movieflix/entities/Movie.java
@@ -0,0 +1,44 @@
+package com.movieflix.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer movieId;
+
+    @Column(nullable = false, length = 200)
+    @NotBlank(message = "Please Provide movie's title!")
+    private String title;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please Provide movie's director!")
+    private String director;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please Provide movie's studio!")
+    private String studio;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast")
+    private Set<String> movieCast; //collection of string
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's release year!")
+    private Integer releaseYear;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's release year!")
+    private String poster;
+}
